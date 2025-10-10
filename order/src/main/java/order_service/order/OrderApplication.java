8835bfb3c395361ae0f0a17e25a6af449f747228
Shaxriyor <rahimov55shah@gmail.com>
2025-10-10 12:39:22 +0500

    payment

--- order/src/main/java/order_service/order/OrderApplication.java
+++ order/src/main/java/order_service/order/OrderApplication.java
@@ -6,7 +6,7 @@ import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
 @EnableCaching
-public class OrderApplication {
+public class  OrderApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(OrderApplication.class, args);
